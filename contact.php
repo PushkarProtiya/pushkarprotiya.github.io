28935be0e6f34568a3db064d0d6a15e72036a3c5
Pushkar Protiya <101090951+pushkarprotiya@users.noreply.github.com>
2022-10-15 11:32:32 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,14 +9,14 @@
 
     $to = "pushkarprotiya@gmail.com";
     $subject = "mail from portfolio";
-    $text = "name: ".$name."\r\n"."Email: ".$email."\r\n"."text: ".$textarea."\r\n";
+    $text = "name: ".$name."\r\n"."email: ".$email."\r\n"."text: ".$textarea."\r\n";
 
     $headers = "From:".$email;
     $header .= "MIME-Version: 1.0\r\n";
     $header .= "Contact-type: text/html/r/n";
 
     if(mail($to,$subject,$text,$headers)){
-        header('location: check-eligibilty-step2.php');
+        header('location: index.html');
     }
     else{
         echo "mail not send";
